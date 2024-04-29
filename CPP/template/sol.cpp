ecfd24024ecb689aed3b074d700c7c07a6defac5
Hanseong <phs7646@kaist.ac.kr>
2024-04-29 22:05:40 +0900

    0429

--- CPP/template/sol.cpp
+++ CPP/template/sol.cpp
@@ -3,15 +3,24 @@
 
 using namespace std;
 
-int main() {
-
-    return 0;
-}
-
-/* helpers */
+/* helper functions */
 
 template<typename T>
 void vectorprint(vector<T>& v) {
     for(T t : v) cout << t << " ";
     cout << "\n";
+}
+
+template<typename T>
+vector<T> vectorinput(int n) {
+    vector<T> ret(n);
+    for(int i = 0;i < n;i++) cin >> ret[i];
+    return ret;
+}
+
+/* codes */
+
+int main() {
+
+    return 0;
 }
--- template.cpp
+++ template.cpp
@@ -1,26 +1,23 @@
-#include<iostream>
-#include<vector>
-
+#include "iostream"
+#include "string"
 using namespace std;
 
+string ReverseFunction(int array[2], const string& InputStr) {
+    int a = array[0];
+    int b = array[1];
+    string retStr = InputStr;
+    for(int i = 0; i < b-a;i++) {
+        retStr[a + i] = InputStr[b-i];
+    }
+   return retStr;
+}
+
 int main() {
-
+    string str;
+    int reverseNum[][2] = { {0,7},{4,6},{5,14},{2,7} };
+    getline(cin, str);
+    for (int index = 0; index < 4; index++) {
+        cout << ReverseFunction(reverseNum[index], str) << endl;
+    }
     return 0;
-}
-
-
-
-/* helpers */
-
-template<typename T>
-vector<T> vectorinput(T n) {
-    vector<T> ret(n);
-    for(int i = 0;i < n;i++) cin >> ret[i];
-    return ret;
-}
-
-template<typename T>
-void vectorprint(vector<T>& v) {
-    for(T t : v) cout << t << " ";
-    cout << "\n";
 }
