1e2e053ac629d9868632edcbdc59fcc7c26841c3
Hanseong <phs7646@kaist.ac.kr>
2024-07-08 23:30:52 +0900

    add readme

--- swea/20671/sol3.cpp
+++ swea/20671/sol3.cpp
@@ -9,19 +9,7 @@ long long mod = 1000000007;
 bool customCompare(const pair<string, int>& a, const pair<string, int>& b) {
     return a.first + b.first < b.first + a.first;
 }
-long long calculate(int l) {
-    // 10^l 계산
-    long long result = 1;
-    long long base = 10;
-    while(l > 0) {
-        if(l % 2 == 1) {
-            result = (result * base) % mod;
-        }
-        base = (base * base) % mod;
-        l /= 2;
-    }
-    return result;
-}
+
 long long stringToModInt(const string &s) {
     long long ret = 0;
     for (char digit : s) {
@@ -47,7 +35,6 @@ int main() {
         }
         sort(arr.begin(),arr.end(),customCompare);
         int _N = 2 * N;
-        //long long answer = calculate1(N-2); //(10^(2^(N-2))) 계산
         long long answer = 0;
         vector<pair<int,int>> possib(N,{0,0});
         for(int i = _N-1;i >= 0;i--) {
@@ -55,8 +42,6 @@ int main() {
             for(int j = 0;j < N;j++) {
                 //자신 제외
                 if(j != arr[i].second) {
-                    //long long left = calculate(possib[j].first);
-                    //long long right = calculate(possib[j].second);
                     long long left = tenof[possib[j].first];
                     long long right = tenof[possib[j].second];
                     long long inter = (left+right)%mod;
