4bbeb4d080858b13cd3508b7fc1c6a0db0c9463a
phs7646 <phs7646@gmail.com>
2024-09-11 17:55:05 +0900

    0911

--- JAVA/sweaProbs/etc/Main.java
+++ JAVA/sweaProbs/etc/Main.java
@@ -1,84 +1,80 @@
-package day0910;
+package etc;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
 
-public class A형 {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine().trim());
+public class Main {
+	static int INF = 100000000;
 
-        for (int t = 1; t <= T; t++) {
-            int N = Integer.parseInt(br.readLine().trim());
-
-            List<Integer> adj[] = new ArrayList[N + 1];
-            for (int i = 0; i < N + 1; i++) {
-                adj[i] = new ArrayList<>();
-            }
-
-            int arr[] = new int[N + 1];
-            for (int i = 1; i <= N; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine().trim());
-                arr[i] = Integer.parseInt(st.nextToken());
-                for (int j = 0; j < arr[i]; j++) {
-                    int num = Integer.parseInt(st.nextToken());
-                    adj[num].add(i);
-                }
-            }
-            // input end
-
-//            int z = 0;
-//            for (List<Integer> a : adj) {
-//                System.out.println("adj "+ z++);
-//                for (int i = 0; i < a.size(); i++) {
-//                    System.out.print(a.get(i) + " ");
-//                }
-//                System.out.println(" adj size " + a.size());
-//            }
-
-            int answer = 0;
-            int cnt = 0;
-            int add = 0;
-            while (true) {
-                List<Integer> tmp = new ArrayList<>();
-                boolean flag = false;
-                for (int i = 1; i < N + 1; i++) {
-                    if (arr[i] == 0) {
-                        for (int j = 0; j < adj[i].size(); j++) {
-                            int num = adj[i].get(j);
-//                            System.out.println("num " + num);
-                            tmp.add(num);
-                        }
-                        arr[i] = -1;
-                        cnt++;
-                        flag = true;
-//                        System.out.println("true");
-//                        System.out.println(Arrays.toString(arr));
-                    }
-                }
-//                for (Integer a : tmp) {
-//                    System.out.print(a + " ");
-//                }
-                if (!flag) {
-                    answer = -1;
-                    break;
-                }
-                for (int i = 0; i < tmp.size(); i++) {
-                    arr[tmp.get(i)]--;
-                }
-                answer++;
-                if (cnt == N) {
-                    break;
-                }
-            }
-            sb.append("#").append(t).append(" ").append(answer).append('\n');
-        }
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		if (N >= K) {
+			System.out.println(N - K);
+			return;
+		}
+		int dist[] = new int[K + 1];
+		for (int i = 0; i <= K; i++)
+			dist[i] = INF;
+		int answer = INF;
+		Queue<Integer> q = new ArrayDeque<>();
+		int it = N;
+		while(true) {
+			if(it == K) {
+				System.out.println(0);
+				return;
+			} else if(it > K) {
+				answer = Math.min(answer, it-K);
+				break;
+			} else { //left < K
+				dist[it] = 0;
+				q.add(it);
+				it <<= 1; //*2
+				if(it == 0) break;
+			}
+		}
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			if(dist[cur]+1 > answer) break;
+			int d = dist[cur] + 1;
+			if (cur >= 2) {
+				int left = cur-1;
+				while(true) {
+					if(left == K) {
+						System.out.println(d);
+						return;
+					} else if(left > K) {
+						answer = Math.min(answer, d + left-K);
+						break;
+					} else { //left < K
+						if(d < dist[left]) {
+							dist[left] = d;
+							q.add(left);
+						}
+						left <<= 1; //*2
+					}
+				}
+			}
+			int right = cur+1;
+			while(true) {
+				if(right == K) {
+					System.out.println(d);
+					return;
+				} else if(right > K) {
+					answer = Math.min(answer, d + right-K);
+					break;
+				} else { //left < K
+					if(d < dist[right]) {
+						dist[right] = d;
+						q.add(right);
+					}
+					right <<= 1; //*2
+				}
+			}
+		}
+		System.out.println(answer);
+	}
 }
